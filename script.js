5a3025efd61e3ec910283f1ebd926d722b2b3d08
GiddyMwas780 <gideon1mwangi@gmail.com>
2025-08-01 23:03:53 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Typing animation
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    strings:["","Web Designer","Web Developer","Graphic Designer","Software engineer"],
     typeSpeed:100,
     Backspeed:60,
     loop:true
